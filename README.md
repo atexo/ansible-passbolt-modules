1253dee85220d716a1cd653c600ea0b7572eb996
Mabihan <jeanrene.robin@gmail.com>
2023-09-14 18:09:48 +0200

    🔐 Passbolt resource module

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ansible-passbolt-modules
 Modules to manage resources, users and folders in Passbolt
+
+## Resources
+Allows you to create, update, delete, and share your passwords in Passbolt.
+
+```yaml
+name: "Create resource in Passbolt"
+passbolt_resources:
+    passbolt_server: https://passbolt.example.com
+    passbolt_admin_user_fingerprint: 66890FKF4DAA4D94FKD842JGI618931835467EDC71FFA
+    passbolt_admin_user_passphrase: adminsecuredpassword
+    passbolt_admin_user_public_key_file: passbolt_public.txt
+    passbolt_admin_user_private_key_file: passbolt_private.txt
+    name: my-api-password
+    content: the-actual-password
+    folder_id: 5b024c98-777e-487e-99f1-0bf75de3f685
+    state: present
+```
---
+++ passbolt_resource.py
@@ -0,0 +1,189 @@
+#!/usr/bin/python
+
+# Copyright: (c) 2023, Jean-René Robin <jean-rene.robin@atexo.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = r'''
+---
+module: passbolt_resource
+
+short_description: Manage RatticDB Resources
+
+# If this is part of a collection, you need to use semantic versioning,
+# i.e. the version is of the form "2.5.0" and not "2.4".
+version_added: "1.0.0"
+
+description: Allows you to create, update, delete, and share your passwords in Passbolt.
+
+options:
+    passbolt_server:
+        description: The passbolt server URL
+        required: true
+        type: str
+    passbolt_admin_user_fingerprint:
+        description: Fingerprint of the user used to exchange data with Passbolt.
+        required: true
+        type: str
+    passbolt_admin_user_passphrase:
+        description: Passphrase of the user used to exchange data with Passbolt.
+        required: true
+        type: str
+    passbolt_admin_user_public_key_file:
+        description: Path to the public key file of the user used to exchange data with Passbolt.
+        required: true
+        type: str
+    passbolt_admin_user_private_key_file:
+        description: Path to the private key file of the user used to exchange data with Passbolt.
+        required: true
+        type: str
+    name:
+        description: Name of the resource
+        required: true
+        type: str
+    content:
+        description: Content (password) of the resource
+        required: true
+        type: str
+    folder_id:
+        description: Identifier (UUID) of the folder where resource should be uploaded.
+        required: true
+        type: str
+    state:
+        description: Define the state of the resource.
+        choices:
+          - present
+          - absent
+        required: false
+        default: present
+
+author:
+    - Jean-René Robin (@mabihan)
+'''
+
+EXAMPLES = r'''
+# Create a resource
+- name: Test with a message
+  my_namespace.my_collection.my_test:
+    name: hello world
+
+# Read a resource
+- name: Test with a message
+  my_namespace.my_collection.my_test:
+    name: hello world
+
+# Update a resource
+- name: Test with a message and changed output
+  my_namespace.my_collection.my_test:
+    name: hello world
+    new: true
+
+# Delete a resource
+- name: Test failure of the module
+  my_namespace.my_collection.my_test:
+    name: fail me
+'''
+
+RETURN = r'''
+# These are examples of possible return values, and in general should use other names for return values.
+original_message:
+    description: The original name param that was passed in.
+    type: str
+    returned: always
+    sample: 'hello world'
+message:
+    description: The output message that the test module generates.
+    type: str
+    returned: always
+    sample: 'goodbye'
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+import passboltapi
+
+def run_module():
+    # define available arguments/parameters a user can pass to the module
+    module_args = dict(
+        passbolt_server=dict(type='str', required=True),
+        passbolt_admin_user_fingerprint=dict(type='str', required=True),
+        passbolt_admin_user_passphrase=dict(type='str', required=True),
+        passbolt_admin_user_public_key_file=dict(type='str', required=True),
+        passbolt_admin_user_private_key_file=dict(type='str', required=True),
+        name=dict(type='str', required=True),
+        content=dict(type='str', required=True),
+        folder_id=dict(type='str', required=False, default=None),
+        state=dict(type='str', required=True),
+    )
+
+    # seed the result dict in the object
+    # we primarily care about changed and state
+    # changed is if this module effectively modified the target
+    # state will include any data that you want your module to pass back
+    # for consumption, for example, in a subsequent task
+    result = dict(
+        changed=False,
+        original_message='',
+        message=''
+    )
+
+    # the AnsibleModule object will be our abstraction working with Ansible
+    # this includes instantiation, a couple of common attr would be the
+    # args/params passed to the execution, as well as if the module
+    # supports check mode
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    # if the user is working with this module in only check mode we do not
+    # want to make any changes to the environment, just return the current
+    # state with no modifications
+    if module.check_mode:
+        module.exit_json(**result)
+
+    # manipulate or modify the state as needed (this is going to be the
+    # part where your module will do what it needs to do)
+    result['original_message'] = module.params['name']
+    result['message'] = 'goodbye'
+
+
+    # prepare configuration dictionary
+    config = {
+        "SERVER": module.params['passbolt_server'],
+        "USER_FINGERPRINT": module.params['passbolt_admin_user_fingerprint'],
+        "PASSPHRASE": module.params['passbolt_admin_user_passphrase'],
+        "USER_PUBLIC_KEY_FILE": module.params['passbolt_admin_user_public_key_file'],
+        "USER_PRIVATE_KEY_FILE": module.params['passbolt_admin_user_private_key_file'],
+    }
+
+    with passboltapi.PassboltAPI(config=config, new_keys=True) as passbolt:
+      
+      # handle resource creation or update
+      if module.params['state'] == 'present':
+        print('Try to get resource in passbolt. Create it if found, or update it.')
+      elif module.params['state'] == 'absent':
+        print('Try to get resource in passbolt. Delete it if found.')
+
+    # use whatever logic you need to determine whether or not this module
+    # made any modifications to your target
+    if module.params['new']:
+        result['changed'] = True
+
+    # during the execution of the module, if there is an exception or a
+    # conditional state that effectively causes a failure, run
+    # AnsibleModule.fail_json() to pass in the message and the result
+    if module.params['name'] == 'fail me':
+        module.fail_json(msg='You requested this to fail', **result)
+
+    # in the event of a successful module execution, you will want to
+    # simple AnsibleModule.exit_json(), passing the key/value results
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
